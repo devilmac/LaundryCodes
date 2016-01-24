08c219bfa58f36a09e4c36e4e15c770f596a3022
devilmac <chicco.colantoni@gmail.com>
2016-01-24 01:04:06 +0100

    Removed dependency for Lombok.

--- app/src/main/java/com/colantoni/federico/projects/tagcodedress/model/LaundryCodeItem.java
+++ app/src/main/java/com/colantoni/federico/projects/tagcodedress/model/LaundryCodeItem.java
@@ -1,8 +1,5 @@
 package com.colantoni.federico.projects.tagcodedress.model;
 
-import lombok.Data;
-
-@Data
 public class LaundryCodeItem {
 
     private int imageRes;
@@ -10,4 +7,59 @@ public class LaundryCodeItem {
     private String name;
 
     private String description;
+
+    public LaundryCodeItem() {}
+
+    public int getImageRes() {return this.imageRes;}
+
+    public void setImageRes(int imageRes) {this.imageRes = imageRes;}
+
+    public String getName() {return this.name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public String getDescription() {return this.description;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public boolean equals(Object o) {
+
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof LaundryCodeItem)) {
+            return false;
+        }
+        final LaundryCodeItem other = (LaundryCodeItem) o;
+        if (!other.canEqual(this)) {
+            return false;
+        }
+        if (this.imageRes != other.imageRes) {
+            return false;
+        }
+        final Object this$name = this.name;
+        final Object other$name = other.name;
+        if (this$name == null ? other$name != null : !this$name.equals(other$name)) {
+            return false;
+        }
+        final Object this$description = this.description;
+        final Object other$description = other.description;
+        return this$description == null ? other$description == null : this$description.equals(other$description);
+    }
+
+    public int hashCode() {
+
+        final int PRIME = 59;
+        int result = 1;
+        result = result * PRIME + this.imageRes;
+        final Object $name = this.name;
+        result = result * PRIME + ($name == null ? 0 : $name.hashCode());
+        final Object $description = this.description;
+        result = result * PRIME + ($description == null ? 0 : $description.hashCode());
+        return result;
+    }
+
+    protected boolean canEqual(Object other) {return other instanceof LaundryCodeItem;}
+
+    public String toString() {return "com.colantoni.federico.projects.tagcodedress.model.LaundryCodeItem(imageRes=" + this.imageRes + ", name=" + this.name + ", description=" + this.description + ")";}
 }
--- app/src/main/java/com/colantoni/federico/projects/tagcodedress/model/LaundryCodeItemList.java
+++ app/src/main/java/com/colantoni/federico/projects/tagcodedress/model/LaundryCodeItemList.java
@@ -4,12 +4,11 @@ import com.bignerdranch.expandablerecyclerview.Model.ParentListItem;
 
 import java.util.List;
 
-import lombok.Data;
-
-@Data
 public class LaundryCodeItemList implements ParentListItem {
 
     private List<LaundryCodeItem> laundryCodeItemList;
+
+    public LaundryCodeItemList() {}
 
     @Override
     public List<LaundryCodeItem> getChildItemList() {
@@ -22,4 +21,38 @@ public class LaundryCodeItemList implements ParentListItem {
 
         return false;
     }
+
+    public List<LaundryCodeItem> getLaundryCodeItemList() {return this.laundryCodeItemList;}
+
+    public void setLaundryCodeItemList(List<LaundryCodeItem> laundryCodeItemList) {this.laundryCodeItemList = laundryCodeItemList;}
+
+    public boolean equals(Object o) {
+
+        if (o == this) {
+            return true;
+        }
+        if (!(o instanceof LaundryCodeItemList)) {
+            return false;
+        }
+        final LaundryCodeItemList other = (LaundryCodeItemList) o;
+        if (!other.canEqual(this)) {
+            return false;
+        }
+        final Object this$laundryCodeItemList = this.laundryCodeItemList;
+        final Object other$laundryCodeItemList = other.laundryCodeItemList;
+        return this$laundryCodeItemList == null ? other$laundryCodeItemList == null : this$laundryCodeItemList.equals(other$laundryCodeItemList);
+    }
+
+    public int hashCode() {
+
+        final int PRIME = 59;
+        int result = 1;
+        final Object $laundryCodeItemList = this.laundryCodeItemList;
+        result = result * PRIME + ($laundryCodeItemList == null ? 0 : $laundryCodeItemList.hashCode());
+        return result;
+    }
+
+    protected boolean canEqual(Object other) {return other instanceof LaundryCodeItemList;}
+
+    public String toString() {return "com.colantoni.federico.projects.tagcodedress.model.LaundryCodeItemList(laundryCodeItemList=" + this.laundryCodeItemList + ")";}
 }
